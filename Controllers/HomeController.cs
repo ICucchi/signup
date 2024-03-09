43e0241e896b0b8931a5ace1caea727f8e844cf9
Iacopo Cucchi <116788568+ICucchi@users.noreply.github.com>
2024-03-09 08:09:47 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SignUp.Models;
+
+namespace SignUp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private static List<Prodotto> Prodotti = new List<Prodotto>();
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Privacy()
+    {
+        string? nomeUtente = HttpContext.Session.GetString("NomeUtente");
+        if (string.IsNullOrEmpty(nomeUtente))
+        return Redirect("\\home\\SignUp");
+        return View ();
+    }
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Conferma(Prenotazione p){
+    
+        HttpContext.Session.SetString("NomeUtente", p.Nome);
+        return View(p);
+    }
+        public IActionResult Purchase()
+    {
+        return View();
+    }
+        public IActionResult Cart(Prodotto p)
+    {
+        dbContext db = new dbContext();
+        db.Prodotti.Add(p);
+        db.SaveChanges();
+        
+        return View( db.Prodotti.ToList() );
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
